6629555bc59a7a4695ca0fe537ba7e1f1225dcc0
Mohamed Baatour <mohamedbaatourx@gmail.com>
2024-11-13 15:34:35 +0100

    working Search Bar / minor changes

--- home.js
+++ home.js
@@ -1,3 +1,20 @@
 $(".logo").click(function () {
   window.location.href = "home.html";
 });
+
+$(".searchButton").click(function (event) {
+  event.preventDefault();
+
+  var input = $(".searchBar").val().toLowerCase();
+  var name = $(".car-name");
+
+  name.each(function (index, element) {
+    var carNameText = $(element).text().toLowerCase();
+
+    if (carNameText.includes(input)) {
+      $(element).closest(".card").show();
+    } else {
+      $(element).closest(".card").hide();
+    }
+  });
+});
