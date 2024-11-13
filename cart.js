af091b32aaaad872fada8316b1ded0206650d433
Mohamed Baatour <mohamedbaatourx@gmail.com>
2024-11-13 20:28:45 +0100

    Cart Successfully working / made cards dynamic in js instead of HTML Static / minor changes

--- cart.js
+++ cart.js
@@ -1,3 +1,25 @@
 $(".logo").click(function () {
   window.location.href = "home.html";
 });
+
+var cartString = localStorage.getItem("cart") || "[]";
+
+var cartArray = window.JSON.parse(cartString);
+
+console.log(cartArray);
+
+for (i = 0; i < cartArray.length; i++) {
+  $(".cards").append(`<div class="card">
+        <img class="carImg" src="${cartArray[i].image}">
+        <h4 class="car-name">${cartArray[i].name}</h4>
+        <p class="car-disc">${cartArray[i].description}</p>
+        <div class="car-Buttons">
+        <button type="button" class="car-details">Details</button>
+    </div>
+    </div>`);
+}
+
+$(".clear").click(function () {
+  localStorage.removeItem("cart");
+  window.location.reload()
+});
--- home.js
+++ home.js
@@ -1,10 +1,105 @@
+var cars = [
+  {
+    name: "Mercedes AMG A35",
+    image:
+      "https://www.mercedes-amg.com/dam/hq/models/Vehicles/A-class/hatchback-new/mercedes-amg-w177-sspip143111-1440x810.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg.image_file.820.461.1704903499885.file/cq5dam.web.1280.1280.jpg",
+    description:
+      "A compact powerhouse with 302 horsepower, sharp handling, and AMG styling, perfect for urban driving and spirited escapes.",
+  },
+  {
+    name: "Mercedes AMG C63",
+    image:
+      "https://www.mercedes-amg.com/dam/hq/models/Vehicles/C-class/Sedan/c63-s-e-performance/mercedes-amg-w206-sspip141264-1440x810.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg.image_file.820.461.1704903627972.file/cq5dam.web.1280.1280.jpg",
+    description:
+      "The AMG C63 delivers thrilling performance with a 4.0L V8 engine, producing 469 horsepower. Its aggressive design and unmatched power make it a true sports sedan.",
+  },
+  {
+    name: "Mercedes AMG C43",
+    image:
+      "https://www.mercedes-amg.com/dam/hq/models/Vehicles/C-class/Sedan/c63-s-e-performance/mercedes-amg-s206-sspip141266-1440x810.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg.image_file.820.461.1704903607149.file/cq5dam.web.1280.1280.jpg",
+    description:
+      "A blend of performance and practicality, the AMG C43 Estate features a 3.0L V6 engine with 385 horsepower, offering both speed and ample cargo space.",
+  },
+  {
+    name: "Mercedes AMG GLC 43 SUV",
+    image:
+      "https://www.mercedes-amg.com/dam/hq/models/Vehicles/GLC/SUV/glc_suv_neu/ssp-freisteller/mercedes-amg-glc-63-se-01-ext-sspip-148452-1440x810.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg.image_file.820.461.1721891451263.file/cq5dam.web.1280.1280.jpg",
+    description:
+      "The AMG GLC 43 SUV combines luxury with performance, featuring a 3.0L V6 engine producing 385 horsepower, perfect for both dynamic driving and everyday versatility.",
+  },
+  {
+    name: "Mercedes AMG GLC 43 Coupe",
+    image:
+      "https://www.mercedes-amg.com/dam/hq/models/Vehicles/GLC/Coupe/glc_coupe_aej_23_2/mercedes-amg-c254-sspip155871-1440x810.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg.image_file.820.461.1695377149759.file/cq5dam.web.1280.1280.jpg",
+    description:
+      "The AMG GLC 43 Coupe offers a powerful 3.0L V6 engine with 385 horsepower, blending aggressive styling with high-performance handling for an exhilarating driving experience.",
+  },
+  {
+    name: "Mercedes AMG GLS 63",
+    image:
+      "https://www.mercedes-amg.com/dam/hq/models/Modeloverview/bodytype/mercedes-amg-modeloverview-gls-suv-gls63-X167-SSPIP105810-1440x810.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg.image_file.820.461.1611236699159.file/cq5dam.web.1280.1280.jpg",
+    description:
+      "The AMG GLS 63 is a luxury SUV with a 4.0L V8 engine delivering 603 horsepower, offering unmatched power, space, and refinement for those who demand the best.",
+  },
+  {
+    name: "Mercedes AMG ONE",
+    image:
+      "https://www.mercedes-amg.com/dam/hq/models/Vehicles/project-one/mercedes-amg-x1-1440x810(1).jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg.image_file.820.461.1653475414219.file/cq5dam.web.1280.1280.jpg",
+    description:
+      "The AMG ONE is a hypercar that brings Formula 1 technology to the road, featuring a 1.6L turbo V6 hybrid enginewith 1,063 horsepower for unparalleled speed and innovation.",
+  },
+  {
+    name: "Mercedes AMG GT 63 S",
+    image:
+      "https://www.mercedes-amg.com/dam/hq/models/Vehicles/GT/Coupe/gt-neu/mercedes-amg-gt-coupe-sspip152794-c192-1440x810.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg.image_file.820.461.1692193734716.file/cq5dam.web.1280.1280.jpg",
+    description:
+      "The AMG GT 63 S features a 4.0L twin-turbo V8 engine producing 630 horsepower, delivering extraordinary performance, precision, and luxury in a sleek, coupe-style design.",
+  },
+];
+
+var cart = JSON.parse(window.localStorage.getItem("cart") || "[]");
+
+console.log(cart);
+
+var addToCart = function (i) {
+  window.location.href = "cart.html";
+  const car = cars[i];
+  cart.push(car);
+  var convert = JSON.stringify(cart);
+  window.localStorage.setItem("cart", convert);
+};
+
+
+for (i = 0; i < cars.length; i++) {
+  $(".cards").append(`<div class="card">
+        <img class="carImg" src="${cars[i].image}">
+        <h4 class="car-name">${cars[i].name}</h4>
+        <p class="car-disc">${cars[i].description}</p>
+        <div class="car-Buttons">
+        <button type="button" class="car-details" onclick="showDetails">Details</button>
+        <button type="button" class="car-toCart" onclick="addToCart(${i})">Add To Cart</button>
+    </div>
+    </div>`);
+}
+
+//=========================================================
+//                    Navigation
+//=========================================================
+
 
 $(".logo").click(function () {
   window.location.href = "home.html";
 });
 
-$(".searchButton").click(function (event) {
-  event.preventDefault();
+
+//=========================================================
+//                    Search
+//=========================================================
+
+
+
+$(".searchBar").keydown(function () {
+
 
   var input = $(".searchBar").val().toLowerCase();
   var name = $(".car-name");
@@ -18,9 +113,4 @@ $(".searchButton").click(function (event) {
       $(element).closest(".card").hide();
     }
   });
-});
-
-$(".car-toCart").click(function () {
-  $(".card").append($("#cart.html-cart"));
-  window.location.href = "cart.html";
 });
